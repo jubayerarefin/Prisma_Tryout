a4cef20a536f5f2723220679f7882cca66963a17
Sharif Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-07-05 13:45:05 +0600

    - Update package.json - Create Migration - Create Sample Data

--- index.ts
+++ index.ts
@@ -3,6 +3,21 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
+    // Create User
+    // await prisma.user.create({
+    //     data: {
+    //         name: "James",
+    //         email: "james@prisma.io",
+    //         active: true,
+    //         profile: {
+    //             create: {
+    //                 bio: "prodigal son"
+    //             }
+    //         }
+    //     }
+    // })
+
+    //Query User
     const allUsers = await prisma.user.findMany()
     console.log(allUsers)
 }
